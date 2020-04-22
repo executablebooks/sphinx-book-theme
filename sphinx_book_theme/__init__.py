461d2da67952ff580b94a184e4e6313b239800da
Chris Holdgraf <choldgraf@berkeley.edu>
2020-04-22 15:38:36 -0700

    RLS: v0.0.2

--- sphinx_book_theme/__init__.py
+++ sphinx_book_theme/__init__.py
@@ -11,7 +11,7 @@ import sass
 
 from .launch import update_thebelab_context, init_thebelab_core, add_hub_urls
 
-__version__ = "0.0.1"
+__version__ = "0.0.2"
 SPHINX_LOGGER = logging.getLogger(__name__)
 EXTRA_TOC_OPTIONS = {"expand_sections": directives.flag}
 
