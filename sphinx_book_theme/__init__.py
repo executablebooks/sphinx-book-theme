56e50abeac85b75b789c98b8cd0c1289855516ba
Chris Holdgraf <choldgraf@berkeley.edu>
2020-06-11 11:36:15 -0700

    RLS: 0.0.23

--- sphinx_book_theme/__init__.py
+++ sphinx_book_theme/__init__.py
@@ -8,7 +8,7 @@ import sass
 
 from .launch import update_thebelab_context, init_thebelab_core, add_hub_urls
 
-__version__ = "0.0.23dev0"
+__version__ = "0.0.23"
 SPHINX_LOGGER = logging.getLogger(__name__)
 
 
