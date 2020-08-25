4a2cbc905311b3b5338140a70bcec43d68d518ea
Chris Sewell <chrisj_sewell@hotmail.com>
2020-08-25 07:21:37 +0100

    🔧 MAINTAIN: RTD fix

--- setup.py
+++ setup.py
@@ -45,7 +45,7 @@ setup(
             "matplotlib",
             "ipywidgets",
             "pandas",
-            "nbclient~=0.2.0",
+            "nbclient",
             "myst-nb~=0.9.1",
             "sphinx-togglebutton>=0.2.1",
             "sphinx-copybutton",
