8b4eb1fe7bc6bad73615e47c37b3826c6f62394d
AakashGC <aakashgfude@gmail.com>
2020-10-15 11:07:58 +1100

    rtd build check

--- docs/conf.py
+++ docs/conf.py
@@ -1,4 +1,5 @@
 # -- Project information -----------------------------------------------------
+import os
 
 project = "Sphinx Book Theme"
 copyright = "2020"
@@ -111,3 +112,4 @@ blog_baseurl = "https://sphinx-book-theme.readthedocs.io"
 fontawesome_included = True
 post_auto_image = 1
 post_auto_excerpt = 2
+execution_show_tb = "READTHEDOCS" in os.environ
