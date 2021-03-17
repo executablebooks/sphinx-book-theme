3b6aa88dbfe54aa48bdf701e788159a52e3d55ed
Chris Holdgraf <choldgraf@berkeley.edu>
2021-03-16 17:37:31 -0700

    Fixing bibtex version

--- setup.py
+++ setup.py
@@ -32,7 +32,7 @@ setup(
         "docutils>=0.15",
         "sphinx>=2,<4",
         "click",
-        "pydata-sphinx-theme~=0.4.1",
+        "pydata-sphinx-theme<0.5",
         "beautifulsoup4>=4.6.1,<5",
         'importlib-resources>=3.0,<3.5; python_version < "3.7"',
     ],
@@ -49,7 +49,7 @@ setup(
             "sphinx-togglebutton>=0.2.1",
             "sphinx-copybutton",
             "plotly",
-            "sphinxcontrib-bibtex@git+https://github.com/mcmtroffaes/sphinxcontrib-bibtex",  # noqa: E501
+            "sphinxcontrib-bibtex~=2.2",
             "sphinx-thebe",
             "ablog~=0.10.13",
         ],
