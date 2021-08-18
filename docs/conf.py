f11411ca222b7dd4d91ff4e54002f1f1eb9fe252
Chris Sewell <chrisj_sewell@hotmail.com>
2021-08-18 20:44:34 +0200

    Update docs/conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -6,7 +6,7 @@ copyright = "2020"
 author = "the Executable Book Project"
 
 master_doc = "index"
-language = "fr"
+language = "ar"
 
 # -- General configuration ---------------------------------------------------
 
