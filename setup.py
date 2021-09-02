7bdc1c5ca22b3a71a84b0484cad687a5b6b6a00f
mmcky <mamckay@gmail.com>
2021-09-02 20:49:46 +1000

    Revert "upgrade myst-nb to 0.13"
    This reverts commit 215ddda7a849b10c4a9fb95b0bb97b443bc28777.

--- setup.py
+++ setup.py
@@ -30,7 +30,7 @@ setup(
     install_requires=[
         "beautifulsoup4>=4.6.1,<5",
         "click",
-        "docutils>=0.15,<0.18",
+        "docutils>=0.15",
         'importlib-resources>=3.0,<3.5; python_version < "3.7"',
         "pydata-sphinx-theme~=0.6.0",
         "pyyaml",
@@ -44,7 +44,7 @@ setup(
             "folium",
             "numpy",
             "matplotlib",
-            "myst-nb~=0.13",
+            "myst-nb~=0.11.1",
             "nbclient",
             "pandas",
             "plotly",
@@ -58,7 +58,7 @@ setup(
         ],
         "testing": [
             "coverage",
-            "myst_nb~=0.13",
+            "myst_nb~=0.11.1",
             "pytest~=6.0.1",
             "pytest-cov",
             "pytest-regressions~=2.0.1",
