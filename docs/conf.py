abfe69e2a19a68c53d9d3914da50e2ac25a8b4b4
Chris Holdgraf <choldgraf@berkeley.edu>
2021-10-14 20:51:11 -0700

    DOCS: Note TOC level auto-show (#410)

--- docs/conf.py
+++ docs/conf.py
@@ -123,6 +123,7 @@ html_theme_options = {
     # "extra_footer": "<a href='https://google.com'>Test</a>",  # DEPRECATED KEY
     # "extra_navbar": "<a href='https://google.com'>Test</a>",
     # "show_navbar_depth": 2,
+    "show_toc_level": 2,
 }
 
 # -- ABlog config -------------------------------------------------
