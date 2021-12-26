37f0e356b7134204ba0aaf15046f334d2e2f5c69
AakashGC <aakashguptachoudhury@gmail.com>
2021-12-27 09:08:01 +1100

    ✨ IMPROVE: Styling print to pdf (#438)
    Co-authored-by: Chris Holdgraf <choldgraf@gmail.com>

--- sphinx_book_theme/static/sphinx-book-theme.17e99cb0ad62fb3181c7ff3fb104e51b.js
+++ sphinx_book_theme/static/sphinx-book-theme.17e99cb0ad62fb3181c7ff3fb104e51b.js
@@ -9,6 +9,10 @@ var initTooltips=()=>{$(document).ready(function(){$('[data-toggle="tooltip"]').
 var initTocHide=()=>{var scrollTimeout;var throttle=200;var tocHeight=$("#bd-toc-nav").outerHeight(true)+$(".bd-toc").outerHeight(true);var hideTocAfter=tocHeight+200;var checkTocScroll=function(){var margin_content=$(".margin, .tag_margin, .full-width, .full_width, .tag_full-width, .tag_full_width, .sidebar, .tag_sidebar, .popout, .tag_popout");margin_content.each((index,item)=>{var topOffset=$(item).offset().top-$(window).scrollTop();var bottomOffset=topOffset+$(item).outerHeight(true);var topOverlaps=((topOffset>=0)&&(topOffset<hideTocAfter));var bottomOverlaps=((bottomOffset>=0)&&(bottomOffset<hideTocAfter));var removeToc=(topOverlaps||bottomOverlaps);if(removeToc&&window.pageYOffset>20){$("div.bd-toc").removeClass("show")
 return false}else{$("div.bd-toc").addClass("show")};})};var manageScrolledClassOnBody=function(){if(window.scrollY>0){document.body.classList.add("scrolled");}else{document.body.classList.remove("scrolled");}}
 $(window).on('scroll',function(){if(!scrollTimeout){scrollTimeout=setTimeout(function(){checkTocScroll();manageScrolledClassOnBody();scrollTimeout=null;},throttle);}});}
+var printPdf=(el)=>{let tooltipID=$(el).attr("aria-describedby")
+let tooltipTextDiv=$("#"+tooltipID).detach()
+window.print()
+$("body").append(tooltipTextDiv)}
 var initThebeSBT=()=>{var title=$("div.section h1")[0]
 if(!$(title).next().hasClass("thebe-launch-button")){$("<button class='thebe-launch-button'></button>").insertAfter($(title))}
 initThebe();}
