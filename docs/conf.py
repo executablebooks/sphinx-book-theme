54eeebbe5e97ff652aad7d77408e20f2273790d8
AakashGC <aakashguptachoudhury@gmail.com>
2022-11-30 23:27:34 +1100

    extension added for syntax highlighting

--- docs/conf.py
+++ docs/conf.py
@@ -31,6 +31,7 @@ extensions = [
     "sphinxext.opengraph",
     # For the kitchen sink
     "sphinx.ext.todo",
+    "IPython.sphinxext.ipython_console_highlighting",
 ]
 
 # Add any paths that contain templates here, relative to this directory.
