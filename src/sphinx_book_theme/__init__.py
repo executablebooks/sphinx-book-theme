23f6936ae956428463f2ec97eebc2fe68bdd344b
Angus Hollands <goosey15@gmail.com>
2025-02-20 16:20:37 +0000

    chore: bump version

--- src/sphinx_book_theme/__init__.py
+++ src/sphinx_book_theme/__init__.py
@@ -24,7 +24,7 @@ from .header_buttons.source import add_source_buttons
 from ._compat import findall
 from ._transforms import HandleFootnoteTransform
 
-__version__ = "1.1.3"
+__version__ = "1.1.4"
 """sphinx-book-theme version"""
 
 SPHINX_LOGGER = logging.getLogger(__name__)
