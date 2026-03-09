92106a5af2c60dce2687d394eda4525c8b3bb689
Brigitta Sipőcz <bsipocz@gmail.com>
2026-03-09 16:11:42 -0700

    MAINT: default branch has been renamed to main

--- docs/conf.py
+++ docs/conf.py
@@ -109,7 +109,7 @@ thebe_config = {
 html_theme_options = {
     "path_to_docs": "docs",
     "repository_url": "https://github.com/executablebooks/sphinx-book-theme",
-    "repository_branch": "master",
+    "repository_branch": "main",
     "launch_buttons": {
         "binderhub_url": "https://mybinder.org",
         "colab_url": "https://colab.research.google.com/",
